f6c0fddfffca5e992b2646a677dfd451ef88c13c
Rocio Avila <Rocio Avila>
2017-04-19 13:50:47 -0500

    primer esquema

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+var rojo = document.getElementById("rojo");
+var morado = document.getElementById("morado");
+var amarillo = document.getElementById("amarillo");
+
+rojo.style.position = "absolute";
+rojo.style.background = "red";
+rojo.style.width = "400px";
+rojo.style.height = "400px";
+rojo.style.padding = "50px";
+
+morado.style.position = "relative";
+morado.style.background = "purple";
+morado.style.width = "300px";
+morado.style.height = "300px";
+morado.style.top = "40px";
+morado.style.left = "40px";
+
+amarillo.style.position = "relative";
+amarillo.style.background = "yellow";
+amarillo.style.width = "150px";
+amarillo.style.height = "150px";
+
+
+
+/*contenedor.style.position = "relative";
+contenedor.style.background = "DarkGray";
+contenedor.style.width = "600px";
+contenedor.style.padding = "50px";*/
+
+/*para que funcione esta parte se le debe borrar todo dato de coordenadas en css,
+solo se le deja el background-color*/
+/*azul.style.position = "absolute";
+azul.style.top = "250px";
+azul.style.right = "0px";*/
